aabe2d3f6fd5e7b55ce07476f4329cf20328ccd3
Roger Garcia Ferré <rgf1042@gmail.com>
2017-05-16 00:15:01 +0200

    Implementacio login

--- js/sbd/login.js
+++ js/sbd/login.js
@@ -4,26 +4,31 @@ function authenticate() {
     input["password"] = $("#password-input").val();
 
     $.ajax({
-    type: "POST",
-    url: "http://127.0.0.1/api/signin",
-    cache: false,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    data: JSON.stringify(input),
-    error: function () {
-        return false;
-    },
-    success: function (result) {
-        if (result.success == true) {
-            
-        }
-        else {
-            //Error
+        type: "POST",
+        url: "http://127.0.0.1/api/signin",
+        cache: false,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify(input),
+        error: function () {
             return false;
+        },
+        success: function (result) {
+            if (result.success == true) {
+                
+            }
+            else {
+                //Error
+                return false;
+            }
+            return true;
         }
-        return true;
-    }
-});
+    });
+}
+
+function goToIndex() {
+    var url = "index.html?name=" + encodeURIComponent($("#txtName").val()) + "&technology=" + encodeURIComponent($("#ddlTechnolgy").val());
+    window.location.href = url;
 }
 
 $(document).ready(function(){
